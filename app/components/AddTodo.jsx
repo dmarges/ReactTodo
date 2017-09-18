3802ee258f08c4a40a30ddc922d6d044d8a00ba5
djm <donniemarges@gmail.com>
2017-09-18 13:33:46 -0700

    added styling for entire app

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -15,7 +15,7 @@ var AddTodo = React.createClass({
   },
   render: function() {
     return (
-      <div>
+      <div className="container__footer">
         <form onSubmit={this.onSubmit}>
           <input type="text" ref="newTodo" placeholder="What needs to be done?" />
           <button className="button expanded">Add Todo</button>
